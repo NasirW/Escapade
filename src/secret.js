c5fed5ce9ef095bc9df694bd5ee6395b8429998a
Coding Rooms <git@codingrooms.io>
2021-11-10 19:24:52 +0000

    Setting up demo1

--- src/secret.js
+++ src/secret.js
@@ -16,8 +16,8 @@ var getAPIusername = ""; // fill in with your name (if left blank, assuming usin
 // (2) Hardcode your API key information here
 
 // AIRTABLE SUPPORT
-var airtable_api = "key" + "XXXXXXXXXXXXXX";     // fill in your info
-var airtable_baseid = "app" + "YYYYYYYYYYYYYY";  // fill in your info
+var airtable_api = "key" + "zw9iFDJ8ZUZuWi";     // fill in your info
+var airtable_baseid = "app" + "ifC7PasBDt8E1Q";  // fill in your info
 
 // SYSTEMLINK SUPPORT
 var systemlink_api = "XXXXXXXXXXXXXXXXXX"; // fill in your info
