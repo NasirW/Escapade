50e95b3b2295e7f762e3b001901db1fd546e9253
Coding Rooms <git@codingrooms.io>
2021-12-10 11:08:40 +0000

    changed CSS, tried to add player name, but failed

---
+++ src/new.js
@@ -0,0 +1,10 @@
+var input = "Player";
+var document = Escapade/Escapade_remote2.HTML
+function name()
+{
+   input = document.getElementById("pName");
+   var str = "Oh no! You are Stuck in a maze, " + input + "!!";
+    document.getElementById('text').innerHTML = str;
+
+}
+
